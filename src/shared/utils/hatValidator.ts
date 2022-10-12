735b9f787ae5c1229c94f36a446d191cb0a6bcc6
Miguel <miguelenrique1805@gmail.com>
2022-10-11 23:44:44 -0500

    updated hatValidator state_payment

--- src/shared/utils/hatValidator.ts
+++ src/shared/utils/hatValidator.ts
@@ -1,4 +1,4 @@
-import * as yup from "yup";
+import * as yup from 'yup';
 
 export const hatSchema = yup.object({
   body: yup.object({
@@ -6,17 +6,17 @@ export const hatSchema = yup.object({
     color_hat: yup.string(),
     cintillo: yup
       .string()
-      .oneOf(["si", "Si", "no", "No", ""], 'Solo escriba "si" ó "no"'),
+      .oneOf(['si', 'Si', 'no', 'No', ''], 'Solo escriba "si" ó "no"'),
     tafalete: yup
       .string()
-      .oneOf(["si", "Si", "no", "No", ""], 'Solo escriba "si" ó "no"'),
+      .oneOf(['si', 'Si', 'no', 'No', ''], 'Solo escriba "si" ó "no"'),
     measure: yup.string(),
     color_tape: yup.string(),
     size: yup.string(),
     state: yup
       .string()
       .oneOf(
-        ["1", "2", "3", "4", "5", ""],
+        ['1', '2', '3', '4', '5', ''],
         'Solo escriba "1", "2", "3", "4" ó "5"'
       ),
     price: yup.string(),
@@ -25,6 +25,6 @@ export const hatSchema = yup.object({
     observations: yup.string(),
     state_payment: yup
       .string()
-      .oneOf(["c", "p", "C", "P", ""], 'Solo escriba "p" ó "c"'),
+      .oneOf(['c', 'p', 't', 'C', 'P', 'T', ''], 'Solo escriba "p" ó "c"'),
   }),
 });
